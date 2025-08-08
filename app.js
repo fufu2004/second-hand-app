31558d9b9065068d19bc8dca0398a72329ce56e4
fufu2004 <110229979+fufu2004@users.noreply.github.com>
2025-08-08 17:08:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -639,7 +639,7 @@ document.addEventListener('DOMContentLoaded', () => {
         installAppContainer.innerHTML = '';
 
         const installButtonHtml = `
-            <button id="install-app-btn" class="flex items-center gap-2 bg-amber-400 text-gray-800 font-bold py-2 px-3 rounded-full shadow-md transition transform hover:scale-105" title="להורדת האפליקציה">
+            <button id="install-app-btn" class="flex items-center gap-2 bg-amber-400 text-gray-800 font-bold py-2 px-3 rounded-full shadow-md transition transform hover:scale-105" title="להורדת האפליקקציה">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
                 <span class="text-sm">להורדת האפליקציה</span>
             </button>
@@ -2058,6 +2058,10 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             case 'show-chat-list': {
                 showChatView();
+                break;
+            }
+             case 'show-admin-view': {
+                showView('admin-view');
                 break;
             }
             case 'retry-fetch': {
